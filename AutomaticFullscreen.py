12ebe7e8f0f97813afe45768be3b3a646fd7040e
lefoy <me@lefoy.net>
2014-12-09 12:56:47 -0500

    Revert last commit

--- AutomaticFullscreen.py
+++ AutomaticFullscreen.py
@@ -2,9 +2,6 @@ import sublime
 import sublime_plugin
 
 
-class CurrentPathStatusCommand(sublime_plugin.EventListener):
-
-    def __init__(self):
-
-        for window in sublime.windows():
-            window.run_command("toggle_full_screen")
+def plugin_loaded():
+    for window in sublime.windows():
+        window.run_command("toggle_full_screen")
